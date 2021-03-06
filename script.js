c5c1e3daf33351f6c621baf6b75d93309ca8dc80
Soreon <soreon@hotmail.fr>
2021-03-06 14:39:32 +0100

    Exceptions

--- script.js
+++ script.js
@@ -3,15 +3,21 @@ xhr.open('GET', 'https://api.github.com/users/Soreon/repos');
 xhr.onload = function () {
     'use script';
     if (xhr.status === 200) {
+        const exceptions = [
+            'project',
+            'lignum'
+        ];
         var repos = JSON.parse(xhr.responseText);
         var ul = document.createElement("ul");
         repos.forEach(function(element) {
-            var li = document.createElement("li");
-            var a = document.createElement("a");
-            a.href = `http://soreon.github.io/${element.name}`;
-            a.textContent = element.name;
-            li.appendChild(a);
-            ul.appendChild(li);
+            if (!exceptions.includes(element.name)) {
+                var li = document.createElement("li");
+                var a = document.createElement("a");
+                a.href = `http://soreon.github.io/${element.name}`;
+                a.textContent = element.name;
+                li.appendChild(a);
+                ul.appendChild(li);
+            }
         }, this);
         document.body.appendChild(ul);
     }
