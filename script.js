464cbf61ed7ab8e8f5ae2ffa6aef103f9cd3dd16
Jean Bachelet <j.bachelet@waigeo.fr>
2022-07-18 14:39:38 +0200

    Màj

--- script.js
+++ script.js
@@ -1,25 +1,15 @@
-var xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://api.github.com/users/Soreon/repos');
-xhr.onload = function () {
-    'use script';
-    if (xhr.status === 200) {
-        const exceptions = [
-            'projects',
-            'lignum'
-        ];
-        var repos = JSON.parse(xhr.responseText);
-        var ul = document.createElement("ul");
-        repos.forEach(function(element) {
-            if (!exceptions.includes(element.name)) {
-                var li = document.createElement("li");
-                var a = document.createElement("a");
-                a.href = `http://soreon.github.io/${element.name}`;
-                a.textContent = element.name;
-                li.appendChild(a);
-                ul.appendChild(li);
-            }
-        }, this);
-        document.body.appendChild(ul);
-    }
-};
-xhr.send(null);
+const exceptions = ['projects', 'lignum'];
+
+fetch('https://api.github.com/users/Soreon/repos')
+  .then((r) => r.json())
+  .then((repos) => {
+    const ul = crel('ul');
+
+    repos.forEach((repo) => {
+      if (exceptions.includes(repo.name)) return;
+
+      const li = crel('li', crel('a', { href: `http://soreon.github.io/${repo.name}` }, repo.name));
+      ul.appendChild(li);
+    });
+    document.body.appendChild(ul);
+  });
